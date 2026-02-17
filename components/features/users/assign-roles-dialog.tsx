3c43695f29bc680c15a980af3c02315d3b5c28b9
ghinaderbes2002 <ghinaderbes@gmail.com>
2026-02-17 12:48:40 +0300

    ...

--- components/features/users/assign-roles-dialog.tsx
+++ components/features/users/assign-roles-dialog.tsx
@@ -32,7 +32,7 @@ export function AssignRolesDialog({ open, onOpenChange, user }: AssignRolesDialo
   useEffect(() => {
     if (user && user.roles) {
       setSelectedRoles(user.roles.map((r) => r.role.id));
-    } else {و
+    } else {
       setSelectedRoles([]);
     }
   }, [user]);
